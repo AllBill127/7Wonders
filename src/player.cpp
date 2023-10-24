0096fcbf2251fe301e54ad944fcc42470604820f
AllBill127 <90201218+AllBill127@users.noreply.github.com>
2023-10-25 01:29:28 +0300

    Update player.cpp
    remove unused code and comment

--- src/player.cpp
+++ src/player.cpp
@@ -240,8 +240,7 @@ bool Player::BuildStructure(DMAG::Card c, std::vector<DMAG::Card>& cards, bool f
             break;
     }
 
-    //cards_hand.erase(cards_hand.begin()+i);
-    cards.erase(cards.begin() + i); //when cards passed as ref using type&
+    cards.erase(cards.begin() + i);
     cards_played.push_back(c);
 
     if (!free_card) this->resources[RESOURCE::coins] -= cost;
